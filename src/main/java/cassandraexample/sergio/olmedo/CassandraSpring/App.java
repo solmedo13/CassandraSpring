96ccbc5e7eb59c65014da14a808b688b09d629f9
solmedo13 <s.olmedo13@gmail.com>
2015-02-21 13:21:34 +0100

    NewCasandra+Spring Example

---
+++ src/main/java/cassandraexample/sergio/olmedo/CassandraSpring/App.java
@@ -0,0 +1,13 @@
+package cassandraexample.sergio.olmedo.CassandraSpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
